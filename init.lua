05bf8e3057e7e765d7693d04a6bbfcff8bc448d3
Preetham <preetham.guduri1@gmail.com>
2026-06-23 23:29:44 -0500

    Added Delete with clipboard overide keybind

--- init.lua
+++ init.lua
@@ -3,39 +3,39 @@ require("config.lazy")
 
 -- Compile & Run file fuction
 local function run_file()
-  local ft = vim.bo.filetype
-  local file = vim.fn.expand("%")
-  local out = vim.fn.expand("%:r")
+	local ft = vim.bo.filetype
+	local file = vim.fn.expand("%")
+	local out = vim.fn.expand("%:r")
 
-  local cmds = {
-    cpp = string.format("g++ -std=c++17 -o %s %s && ./%s", out, file, out),
-    c = string.format("gcc -o %s %s && ./%s", out, file, out),
-    python = string.format("python3 %s", file),
-    javascript = string.format("node %s", file),
-    typescript = string.format("ts-node %s", file),
-    rust = string.format("cargo run"),
-    go = string.format("go run %s", file),
-    sh = string.format("bash %s", file),
-    lua = string.format("lua %s", file),
-  }
-  local cmd = cmds[ft]
-  if not cmd then
-    print("Runner not configured for filetype: " .. ft)
-    return
-  end
+	local cmds = {
+		cpp = string.format("g++ -std=c++17 -o %s %s && ./%s", out, file, out),
+		c = string.format("gcc -o %s %s && ./%s", out, file, out),
+		python = string.format("python3 %s", file),
+		javascript = string.format("node %s", file),
+		typescript = string.format("ts-node %s", file),
+		rust = string.format("cargo run"),
+		go = string.format("go run %s", file),
+		sh = string.format("bash %s", file),
+		lua = string.format("lua %s", file),
+	}
+	local cmd = cmds[ft]
+	if not cmd then
+		print("Runner not configured for filetype: " .. ft)
+		return
+	end
 
-  vim.cmd("write")
-  vim.cmd("botright split | terminal " .. cmd)
-  vim.cmd("startinsert")
+	vim.cmd("write")
+	vim.cmd("botright split | terminal " .. cmd)
+	vim.cmd("startinsert")
 end
 
 -- Close all terminal buffer
 local function closeTerminals()
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.bo[buf].buftype == "terminal" then
-      vim.api.nvim_buf_delete(buf, { force = true })
-    end
-  end
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.bo[buf].buftype == "terminal" then
+			vim.api.nvim_buf_delete(buf, { force = true })
+		end
+	end
 end
 
 ---------------- KEYBINDINGS
